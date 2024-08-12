db01e545d29af7bde492597ad5c39b2575777311
birunthabanr <birunthur@gmail.com>
2024-08-12 23:22:08 +0530

    -

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let board = document.querySelector(".board")
-board.computedStyleMap.gridTemplateColoumns = "repeat(16, 1fr)";
-board.computedStyleMap.gridTemplateRows = "repeat(16, 1fr)";
+board.style.gridTemplateColumns = "repeat(16, 1fr)";
+board.style.gridTemplateRows = "repeat(16, 1fr)";
 
 
 for (let i = 0; i< 256; i++) {
     let square = document.createElement("div");
-    square.style.background = "blue";
-    board.insertAdjacentElement("beforehand", square);
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
 }
